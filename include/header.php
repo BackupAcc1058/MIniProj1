dfa1074e43b8239aec2308b1c71b5a7407277f19
GalihPramana <galih.pramana@ti.ukdw.ac.id>
2025-06-10 23:28:03 +0700

    fix header css

--- include/header.php
+++ include/header.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="css/header.css">
+<link rel="stylesheet" href="css/header2.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"> 
 <?php
     require_once 'config/koneksi.php';
